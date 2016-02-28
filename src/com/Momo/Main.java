c233c9ef8bd7875d7c0e9beb863402096650cc43
Sheveil Johnson <mo12g13@gmail.com>
2016-02-28 00:03:31 -0600

    Successfully completed.

--- src/com/Momo/Main.java
+++ src/com/Momo/Main.java
@@ -1,5 +1,8 @@
 package com.Momo;
 
+import com.sun.xml.internal.ws.api.WSService;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -8,142 +11,286 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        Scanner  input = new Scanner(System.in);
-        ArrayList <String> mynew = new ArrayList<>();
-        int j =0;
-        FileReader readThisFile = new FileReader("coffee.txt");
-        BufferedReader report = new BufferedReader(readThisFile);
-        String line = report.readLine();
-        while (line != null){
-            mynew.add(line);
-            line = report.readLine();
-        }
-        System.out.println("How many cappuccino drinks were sold today?");
-        int cappuccinoDrink = input.nextInt();
-        double capexpenses = cappuccinoDrink*1.56;
-        double capRevenue = cappuccinoDrink*3.50;
-        double capProfit = capRevenue-capexpenses;
+    public static void main(String[] args)throws IOException {
+        Scanner input = new Scanner(System.in);
+        int cappuccinoDrink = 0, espressoDrink=0, latte=0, blackTea=0, herbTea=0, macchiatoTea=0, americano=0, coldPress=0, hotchololate =0;
+        double capexpenses=0, capRevenue=0, capProfit=0, espExpenses=0, espRevenue=0, espProfit=0, herbExpenses=0, herbRevenue=0;
+        double lattExpenses=0, latteRevenue=0, lattProfit=0, blackExpenses=0, blackRevenues=0, blackProfit=0, herbProfit=0, coldProfit=0;
+        double maccExpenses=0, maccRevenue=0, maccProfit=0, ameriExpenses=0, ameriProfit=0, ameriRevenue=0, coldExpenses=0, coldRevenue=0;
+        int coffee=0, chaiTea=0, mocha=0;
+        double hotExpenses=0, hotRevenue=0, hotProfit=0, coffeeExpenses=0, coffeeRevenue=0, coffeeProfit=0, chaiExpenses=0, chaiRevenue=0, chaiProfit=0, mochaExpenses=0, mochProfit=0, mochaRevenue=0;
+        int count = -1;
 
-        System.out.println("How many espresso drinks were sold today?");
-        int espressoDrink = input.nextInt();
-        double espExpenses = espressoDrink*0.95;
-        double  espRevenue = espressoDrink*1.50;
-        double espProfit = espRevenue-espExpenses;
+        ArrayList<String> mynew = new ArrayList<>();
+        boolean bool1 = true, bool2=true, bool3 = true, bool4 = true, bool5=true, bool6 =true, bool7=true;
+        boolean bool8 =  true, bool9=true, bool10=true, bool11 = true, bool12 = true;
+        try {
+
+            FileReader readThisFile = new FileReader("coffee.txt");
+            BufferedReader report = new BufferedReader(readThisFile);
+            String line = report.readLine();
+            while (line != null) {
+                mynew.add(line);
+                line = report.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("File Not Found!");
+        }
+
+
+do {
+    try{
+
+    System.out.println("How many cappuccino drinks were sold today?");
+
+        cappuccinoDrink = input.nextInt();
+        if(cappuccinoDrink>0) {
+            capexpenses = cappuccinoDrink * 1.56;
+            capRevenue = cappuccinoDrink * 3.50;
+            capProfit = capRevenue - capexpenses;
+           bool1 = false;
+        }
+    }catch (InputMismatchException e) {
+
+
+        input.nextLine();
+        System.out.println("Please enter integer");
+       }
+}while (bool1);
+
+        do {
+            try {
+                System.out.println("How many espresso drinks were sold today?");
+                espressoDrink=input.nextInt();
+                if(espressoDrink>0) {
+                    espExpenses = espressoDrink * 0.95;
+                    espRevenue = espressoDrink * 1.50;
+                    espProfit = espRevenue - espExpenses;
+                    bool2 = false;
+                }
+            }catch (InputMismatchException e) {
+                input.nextLine();
+                System.out.println("Please enter an Integer");
+
+            }
+
+
+        }while (bool2);
+
+do {
+    try {
+
 
         System.out.println("How many latte drinks were sold today?");
-        int latte = input.nextInt();
-        double lattExpenses = latte*1.67;
-        double latteRevenue = latte*3.75;
-        double lattProfit = latteRevenue-lattExpenses;
+        latte = input.nextInt();
+        if(latte>0) {
+            lattExpenses = latte * 1.67;
+            latteRevenue = latte * 3.75;
+            lattProfit = latteRevenue - lattExpenses;
+            bool3 = false;
+        }
+    }catch (InputMismatchException e){
+        input.nextLine();
+        System.out.println("Please enter an Integer");
 
-        System.out.println("How many black tea drinks were sold today?");
-        int  blackTea = input.nextInt();
-        double blackExpenses = blackTea*0.35;
-        double blackRevenues = blackTea*1.79;
-        double blackProfit = blackRevenues-blackExpenses;
+    }
+}while(bool3);
 
-        System.out.println("How many herbal tea drinks were sold today?");
-        int herbTea = input.nextInt();
-        double herbExpenses = herbTea*0.35;
-        double herbRevenue = herbTea*2.09;
-        double herbProfit = herbRevenue-herbExpenses;
-
-        System.out.println("How many macchiato drinks were sold today?");
-        int macchiatoTea = input.nextInt();
-        double maccExpenses = macchiatoTea*1.45;
-        double maccRevenue = macchiatoTea*2.75;
-        double maccProfit = maccRevenue-maccExpenses;
-        System.out.println("How many americano drinks were sold today?");
-
-        int americano = input.nextInt();
-        double ameriExpenses = americano* 0.70;
-        double ameriRevenue = macchiatoTea*2.65;
-        double ameriProfit = ameriRevenue-ameriExpenses;
-
-        System.out.println("How many cold press drinks were sold today?");
-        int  coldPress = input.nextInt();
-        double coldExpenses = coldPress*0.50;
-        double coldRevenue = coldPress*2.50;
-        double coldProfit = coldRevenue-coldExpenses;
-
-        System.out.println("How many hot chocolate drinks were sold today?");
-        int hotchololate = input.nextInt();
-        double hotExpenses = hotchololate*0.65;
-        double hotRevenue = herbTea*2.25;
-        double hotProfit = hotRevenue-hotExpenses;
-
-        System.out.println("How many coffee drinks were sold today?");
-        int cofee = input.nextInt();
-        double coffeeExpenses = cofee*0.25;
-        double coffeeRevenue = cofee*1.65;
-        double coffeeProfit = coffeeRevenue-coffeeExpenses;
-
-        System.out.println("How many chai tea were sold today?");
-        int  chaiTea = input.nextInt();
-        double chaiExpenses = chaiTea*1.23;
-        double chaiRevenue = chaiTea* 3.50;
-        double chaiProfit = chaiRevenue-chaiExpenses;
-
-        System.out.println("How many mocha were sold today?");
-        int mocha = input.nextInt();
-        double mochaExpenses = mocha*1.45;
-        double mochaRevenue = mocha*3.70;
-        double mochProfit = mochaRevenue-mochaExpenses;
-        
+        do { try {
 
 
-        FileWriter salereport = new FileWriter("sale.txt");
-        salereport.write(String.format("Cappuccino: Sold %d, Expenses, $%.2f, Revenue $%.2f, Profit $%.2f%n", cappuccinoDrink
-                ,capexpenses, capRevenue, capProfit));
-        salereport.write(String.format("Espresso: Sold %d Expenses $%.2f, Revenue %.2f, Profit $%.2f%n", espressoDrink, espExpenses,
-                espRevenue, espProfit));
+            System.out.println("How many black tea drinks were sold today?");
+            blackTea = input.nextInt();
+            if(blackTea>0){
+            blackExpenses = blackTea * 0.35;
+            blackRevenues = blackTea * 1.79;
+            blackProfit = blackRevenues - blackExpenses;
+            bool4 = false;
+            }
+        }catch (InputMismatchException e){
+            input.nextLine();
+            System.out.println("Please enter an integer");
+        }
+        }while(bool4);
 
-        salereport.write(String.format("Latte: Sold %d, Expenses $%.2f, Revenue $%.2f, Profit $%.2f%n", latte,
-                lattExpenses,latteRevenue, lattProfit ));
+        do {
+            try {
 
-        salereport.write(String.format("Black Tea: Sold %d, Expenses $ %.2f, Revenue $%.2f, Profit $%.2f%n",blackTea
-                ,blackExpenses, blackRevenues, blackProfit));
+                System.out.println("How many herbal tea drinks were sold today?");
+                herbTea = input.nextInt();
+                if(herbTea>0) {
+                    herbExpenses = herbTea * 0.35;
+                    herbRevenue = herbTea * 2.09;
+                    herbProfit = herbRevenue - herbExpenses;
+                    bool5 = false;
+                }
+            }
+            catch (InputMismatchException e){
+                input.nextLine();
+                System.out.println("Please enter intger");
+            }
+        }while(bool5);
 
-        salereport.write(String.format("Herbal tea: Sold %d, Expenses $ %,2f, Revenue $  %.2f" +
-                "Profit $%.2f%n", herbTea,herbExpenses, herbRevenue,  herbProfit));
+        do {
+            try {
 
-        salereport.write(String.format("Macchiato: Sold %d, Expenses $%.2f, Revenue $ %.2f, Profit $ %.2f%n",
-                macchiatoTea, maccExpenses, maccRevenue ,maccProfit));
+                System.out.println("How many macchiato drinks were sold today?");
+                macchiatoTea = input.nextInt();
+                if(macchiatoTea >0) {
+                    maccExpenses = macchiatoTea * 1.45;
+                    maccRevenue = macchiatoTea * 2.75;
+                    maccProfit = maccRevenue - maccExpenses;
+                    bool6= false;
+                }
 
-        salereport.write(String.format("Americano: Sold %d, Expenses $%.2f, Revenue $ %.2f, Profit $ %.2f%n",
-                americano, ameriExpenses, ameriRevenue ,ameriProfit));
+            }catch (InputMismatchException e){
+                input.nextLine();
+                System.out.println("Please enter an integer");
+            }
+        }while (bool6);
 
-        salereport.write(String.format("Cold press: Sold %d, Expenses $%.2f, Revenue, $%.2f, Profit $%.2f%n",
-                coldPress,coldExpenses, coldRevenue,  coldProfit));
+        do {
+            try {
+                System.out.println("How many americano drinks were sold today?");
+                americano = input.nextInt();
+                if(americano>0) {
+                    ameriExpenses = americano * 0.70;
+                    ameriRevenue = americano * 2.65;
+                    ameriProfit = ameriRevenue - ameriExpenses;
+                    bool7 = false;
+                }
+            }catch (InputMismatchException e){
+                input.nextLine();
+                System.out.println("Please enter an integer");
+            }
+        }while (bool7);
+        do {
+            try {
 
-        salereport.write(String.format("Hot chocolate: Sold %d, Expenses $ %.2f, Revenue $ %.2f, Profit $ %.2f%n"
-                , hotchololate, hotExpenses, hotRevenue, hotProfit));
+                System.out.println("How many cold press drinks were sold today?");
+                coldPress = input.nextInt();
+                if(coldPress >0) {
+                    coldExpenses = coldPress * 0.50;
+                    coldRevenue = coldPress * 2.50;
+                    coldProfit = coldRevenue - coldExpenses;
+                    bool8 = false;
+                }
+            }catch (InputMismatchException e){
+                input.nextLine();
+                System.out.println("Please enter an integer");
+            }
+        }while (bool8);
 
-        salereport.write(String.format("Coffee : Sold %d, Expenses $ %.2f, Revenue $ %.2f, Profit $ %.2f%n",cofee,
-                coffeeExpenses, coffeeRevenue, coffeeProfit));
+        do {
+            try {
 
-        salereport.write(String.format("Chai tea : Sold %d, Expenses $%.2f, Revenue $%.2f, Profit $%.2f%n"
-                ,chaiTea, chaiExpenses,chaiRevenue, chaiRevenue));
+                System.out.println("How many hot chocolate drinks were sold today?");
 
-        salereport.write(String.format("Mocha : Sold %d, Expenses $.2%f, Revenue $%.2f, Profit $%.2f%n "
-                , mocha ,mochaExpenses, mochaRevenue, mochProfit));
-        salereport.close();
+                hotchololate = input.nextInt();
+                if(hotchololate>0) {
+                    hotExpenses = hotchololate * 0.65;
+                    hotRevenue = herbTea * 2.25;
+                    hotProfit = hotRevenue - hotExpenses;
+                    bool9=false;
+                }
+            }catch (InputMismatchException e){
+                input.nextLine();
+                System.out.println("Please enter integer");
+            }
+        }while (bool9);
+
+        do {
+            try {
+
+                System.out.println("How many coffee drinks were sold today?");
+
+                    coffee = input.nextInt();
+                    if(coffee>0) {
+                        coffeeExpenses = coffee * 0.25;
+                        coffeeRevenue = coffee * 1.65;
+                        coffeeProfit = coffeeRevenue - coffeeExpenses;
+                        bool10 = false;
+                    }
+
+            }catch (InputMismatchException e){
+                input.nextLine();
+                System.out.println("Please enter an integer");
+            }
+        }while (bool10);
+
+        do {try {
 
 
+            System.out.println("How many chai tea were sold today?");
+            chaiTea = input.nextInt();
+            if(chaiTea>0) {
+                chaiExpenses = chaiTea * 1.23;
+                chaiRevenue = chaiTea * 3.50;
+                chaiProfit = chaiRevenue - chaiExpenses;
+                bool11 = false;
+            }
+        }catch (InputMismatchException e){
+            input.nextLine();
+            System.out.println("Please enter an integer");
+        }
+        }while (bool11);
+
+        do {
+            try {
 
 
+                System.out.println("How many mocha were sold today?");
+
+                mocha = input.nextInt();
+                if(mocha>0){
+                    mochaExpenses = mocha * 1.45;
+                    mochaRevenue = mocha * 3.70;
+                    mochProfit = mochaRevenue - mochaExpenses;
+                    bool12 = false;
+                }
+            }catch (InputMismatchException e){
+                input.nextLine();
+                System.out.println("Please enter an integer");
+            }
+        }while (bool12);
 
 
+                FileWriter salereport = new FileWriter("sale.txt");
+                salereport.write(String.format("Cappuccino: Sold %d, Expenses, $%.2f, Revenue $%.2f, Profit $%.2f%n", cappuccinoDrink
+                        , capexpenses, capRevenue, capProfit));
+                salereport.write(String.format("Espresso: Sold %d Expenses $%.2f, Revenue %.2f, Profit $%.2f%n", espressoDrink, espExpenses,
+                        espRevenue, espProfit));
 
+                salereport.write(String.format("Latte: Sold %d, Expenses $%.2f, Revenue $%.2f, Profit $%.2f%n", latte,
+                        lattExpenses, latteRevenue, lattProfit));
 
+                salereport.write(String.format("Black Tea: Sold %d, Expenses $ %.2f, Revenue $%.2f, Profit $%.2f%n", blackTea
+                        , blackExpenses, blackRevenues, blackProfit));
 
+                salereport.write(String.format("Herbal tea: Sold %d, Expenses $ %,2f, Revenue $  %.2f" +
+                        "Profit $%.2f%n", herbTea, herbExpenses, herbRevenue, herbProfit));
 
+                salereport.write(String.format("Macchiato: Sold %d, Expenses $%.2f, Revenue $ %.2f, Profit $ %.2f%n",
+                        macchiatoTea, maccExpenses, maccRevenue, maccProfit));
 
+                salereport.write(String.format("Americano: Sold %d, Expenses $%.2f, Revenue $ %.2f, Profit $ %.2f%n",
+                        americano, ameriExpenses, ameriRevenue, ameriProfit));
 
+                salereport.write(String.format("Cold press: Sold %d, Expenses $%.2f, Revenue, $%.2f, Profit $%.2f%n",
+                        coldPress, coldExpenses, coldRevenue, coldProfit));
 
+                salereport.write(String.format("Hot chocolate: Sold %d, Expenses $ %.2f, Revenue $ %.2f, Profit $ %.2f%n"
+                        , hotchololate, hotExpenses, hotRevenue, hotProfit));
 
+                salereport.write(String.format("Coffee : Sold %d, Expenses $ %.2f, Revenue $ %.2f, Profit $ %.2f%n", coffee,
+                        coffeeExpenses, coffeeRevenue, coffeeProfit));
 
+                salereport.write(String.format("Chai tea : Sold %d, Expenses $%.2f, Revenue $%.2f, Profit $%.2f%n"
+                        , chaiTea, chaiExpenses, chaiRevenue, chaiRevenue));
+
+                salereport.write(String.format("Mocha : Sold %d, Expenses $.2%f, Revenue $%.2f, Profit $%.2f%n "
+                        , mocha, mochaExpenses, mochaRevenue, mochProfit));
+                salereport.close();
 
 
 
